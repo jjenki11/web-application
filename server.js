9491752e505f3ddfd7c101a59b53365f0fa0aec5
jjenki11 <duderonimy2012@gmail.com>
2021-05-18 23:11:29 -0400

    dpic

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ var app = express();
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/web')));
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + 'index.html');
 });
+
+app.get('/dik_pik', function(req,res) {
+    res.redirect('https://previews.123rf.com/images/capeman29/capeman291906/capeman29190600013/128474478-a-cartoon-penis-with-attitude-wearing-sunglasses-and-giving-a-thumbs-up.jpg');
+    
+})
 
 var server = app.listen(3000, function () {
     console.log('Node server is running..');
